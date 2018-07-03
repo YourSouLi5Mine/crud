d50f4002001a5e1f0eabbd73624679109b5e2cf8
Jorge Hristho Precich Serna <precichysim@gmail.com>
2018-07-03 14:20:27 -0500

    MongoDB and POST

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+var db;
+var url = 'mongodb://Negro:ABC123@ds125821.mlab.com:25821/crud';
+
+MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+	if (err) return console.log(err);
+	db = client.db('crud');
+
+	app.listen(3000, () => {
+		console.log('Listening on 3000');
+	})
+})
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, () => {
-console.log('Listening on 3000');
-})
 
 app.get('/', (req, res) => {
-res.sendFile(__dirname + '/index.html')
+	res.sendFile(__dirname + '/index.html');
+	db.collection('quotes').find().toArray((err, results) => {
+		console.log(results);
+	})
 })
 
 app.post('/quotes', (req, res) => {
-console.log(req.body)
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err);
+		console.log('saved to database');
+		res.redirect('/');
+	})
 })
